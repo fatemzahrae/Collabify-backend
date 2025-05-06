e548cd8a6b157c5b8305a4f45b5d3db8febb38ae
fatemZahrae <kribiif@gmail.com>
2025-05-06 22:02:10 +0000

    add dashboard service and controller"
    "

--- backend/src/main/java/clb/backend/repositories/ProjectRepository.java
+++ backend/src/main/java/clb/backend/repositories/ProjectRepository.java
@@ -1,11 +1,15 @@
 package clb.backend.repositories;
 
 import clb.backend.entities.Project;
+import clb.backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Optional<Project> findByTitle(String title);
+
+    List<Project> findByMembersContaining(User user);
 }
 
